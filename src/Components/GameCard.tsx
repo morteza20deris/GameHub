8adfe45bfca169e2ee29547db6453c7c8e907c7e
morteza20deris <mtz20dri@gmail.com>
2023-08-15 05:48:20 +0330

    Optimized Images

--- src/Components/GameCard.tsx
+++ src/Components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, Image, CardBody, Heading, HStack } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import ImageUrl from "../Services/Image-url";
 
 interface Props {
   game: Game;
@@ -10,7 +11,11 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card>
-      <Image overflow="hidden" borderRadius={10} src={game.background_image} />
+      <Image
+        overflow="hidden"
+        borderRadius={10}
+        src={ImageUrl(game.background_image, 600, 400)}
+      />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
---
+++ src/Services/Image-url.ts
@@ -0,0 +1,12 @@
+
+
+
+const ImageUrl = (url:string,width:number,height:number) => {
+  const target = "media/"
+    const index = url.indexOf(target) + target.length
+    return url.slice(0,index)+"crop/"+width+"/"+height+"/"+url.slice(index)
+    
+  
+}
+
+export default ImageUrl
